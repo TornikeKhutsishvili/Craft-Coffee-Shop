4d3bf8ba815143c91bb5220ac8625d7e76f82c52
Tornike Khutsishvili <tornikexucishvili0@gmail.com>
2026-01-21 22:16:57 +0400

    Add Dashboard

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,24 @@
+const Dashboard = () => {
+  return (
+    <>
+      <div className="main-content">
+        <div className="table-container">
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
