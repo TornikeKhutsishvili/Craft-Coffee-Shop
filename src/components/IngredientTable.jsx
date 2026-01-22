8ca5d09f7c711bdd272ae490fdad72ddd95740bb
Tornike Khutsishvili <tornikexucishvili0@gmail.com>
2026-01-22 18:40:53 +0400

    Modify IngredientForm, IngredientTable and App

--- src/components/IngredientTable.jsx
+++ src/components/IngredientTable.jsx
@@ -1,4 +1,11 @@
+import { useAdminContext } from "../contexts/AdminContext";
+
 const IngredientTable = () => {
+  const { ingredients, deleteIngredients, ingredientsLoading } =
+    useAdminContext();
+
+  if (ingredientsLoading) return <p>Loading . . .</p>;
+
   return (
     <>
       <table>
@@ -12,7 +19,22 @@ const IngredientTable = () => {
             <th>Actions</th>
           </tr>
         </thead>
-        <tbody id="ingredient-table-body"></tbody>
+        <tbody id="ingredient-table-body">
+          {ingredients.map((ingrItem) => (
+            <tr key={ingrItem.id}>
+              <td>{ingrItem.id}</td>
+              <td>{ingrItem.name}</td>
+              <td>$ {ingrItem.price}</td>
+              <td>{ingrItem.strength}</td>
+              <td>{ingrItem.flavor}</td>
+              <td>
+                <button onClick={() => deleteIngredients(ingrItem.id)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </>
   );
